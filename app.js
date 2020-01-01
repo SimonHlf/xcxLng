219f79e0b4f26adc802787e0f3a0efd117195407
simon <simonHlf@163.com>
2020-01-01 22:17:35 +0800

    添加部分模块静态页面
    Signed-off-by: simon <simonHlf@163.com>

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ App({
     
   },
   globalData: {
-	serverUrl : 'http://192.168.1.3:8080'
+	serverUrl : 'http://192.168.1.3:8080',
     userInfo: null,
 	isAuthor:false,//是否已授权
 	wxInfo:null,//用户微信信息
