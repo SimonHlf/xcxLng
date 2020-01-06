6352e9f492ce5d0009ee333acc233ea498e521a4
simon <simonHlf@163.com>
2020-01-06 21:48:31 +0800

    添加储罐买卖发布模块 Signed-off-by: simon <simonHlf@163.com>

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ App({
   },
   globalData: { 
 	provData : [],
-	serverUrl : 'http://192.168.1.3:8888',
-	//serverUrl : 'http://192.168.3.26:8081',
+	//serverUrl : 'http://192.168.1.3:8888',
+	serverUrl : 'http://192.168.3.26:8081',
     userInfo: null,
 	isAuthor:false//是否已授权
   }
